4e192986bc09776f502e4f89575d5c19083d95a8
yahya bnoun <yahyaiko22@gmail.com>
2023-11-15 23:50:02 +0100

    111

--- main.h
+++ main.h
@@ -15,11 +15,13 @@ void executingCommand(char **arr,int status);
 void HandleThePATH(char *name);
 /*
 void HandleTheCommand(char **argv);
+int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+
 */
 
 
-int _strcmp(char *s1, char *s2);
-char *_strcat(char *dest, char *src);
+
 
 
 #endif
